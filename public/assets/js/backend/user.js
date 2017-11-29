9a566f47a21bf73856a637d8d8579f242d4829b0
wangcailin <dreamcailin@hotmail.com>
2017-11-29 22:16:27 +0800

    添加会员系统

---
+++ public/assets/js/backend/user.js
@@ -0,0 +1,56 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'user/index',
+                    add_url: '',
+                    edit_url: 'user/edit',
+                    del_url: 'user/del',
+                    multi_url: 'user/multi',
+                    table: 'user',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'username', title: __('Username')},
+                        {field: 'email', title: __('Email')},
+                        {field: 'mobile', title: __('Mobile')},
+                        {field: 'vip', title: __('Vip')},
+                        {field: 'logintime', title: __('Logintime'), operate: false, visible: false, formatter: Table.api.formatter.datetime},
+                        {field: 'jointime', title: __('Jointime'), operate: false, formatter: Table.api.formatter.datetime},
+                        {field: 'status', title: __('Status'), formatter: Table.api.formatter.status},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
